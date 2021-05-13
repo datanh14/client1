8698faba34428630a7e182a9e6e2bf00191497d0
buivuanh <buivuanh@vnpt.vn>
2021-05-13 10:25:44 +0700

    tttt

--- src/utils/helpers/config.tsx
+++ src/utils/helpers/config.tsx
@@ -3,6 +3,8 @@ export const configs = () => {
     VERSION: "1.0",
     LOGIN_BASE_URL: "http://vuanhlk14-001-site1.itempurl.com",
     BASE_URL: "http://vuanhlk14-001-site1.itempurl.com",
+    //LOGIN_BASE_URL: "http://localhost:61955",
+    //BASE_URL: "http://localhost:61955",
     APP_KEY: "axddsd12fvfaaxxa123faf",
   };
 };
