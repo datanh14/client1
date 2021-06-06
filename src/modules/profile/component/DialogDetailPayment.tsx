18d6897a1ba476513e0fe3ef42b2508e3a3c2524
datanh14 <37982692+datanh14@users.noreply.github.com>
2021-06-06 23:11:10 +0700

    Revert "Revert "lên""
    This reverts commit dacd1476c439e6b4314b0d8b17ff51221a328044.

---
+++ src/modules/profile/component/DialogDetailPayment.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { some } from '../../../constants/constants';
+import JSONbig from 'json-bigint';
+import { Row } from '../../common/Elements';
+import { Avatar, Typography } from '@material-ui/core';
+import { GREY_600 } from '../../../assets/theme/colors';
+import TableCustom from '../../common/TableCustom';
+interface Props {
+  open: boolean;
+  handleClose(): void;
+  dataPayment?: some;
+}
+const DialogDetailPayment: React.FC<Props> = (props) => {
+  const { open, handleClose, dataPayment } = props;
+  const columns = [
+    {
+      title: 'Hình ảnh',
+      dataIndex: 'id',
+      width: 100,
+      styleHeader: { color: GREY_600, textAlign: 'center' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Avatar src={record?.Product?.Images[0]} />
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Tên sản phẩm',
+      dataIndex: 'id',
+      width: 250,
+      styleHeader: { color: GREY_600, textAlign: 'left' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Typography>{record?.Product.Name}</Typography>
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Giá',
+      dataIndex: 'id',
+      width: 150,
+      styleHeader: { color: GREY_600, textAlign: 'left' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Typography>{record?.Product.Price}</Typography>
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Đánh giá',
+      dataIndex: 'id',
+      width: 100,
+      styleHeader: { color: GREY_600, textAlign: 'left' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Typography>{record?.Product.Star}</Typography>
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Lượt đánh giá',
+      dataIndex: 'id',
+      width: 100,
+      styleHeader: { color: GREY_600, textAlign: 'left' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Typography>{record?.Product.RatingsCount}</Typography>
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Size',
+      dataIndex: 'id',
+      width: 100,
+      styleHeader: { color: GREY_600, textAlign: 'left' },
+      render: (record: any) => {
+        return (
+          <Row key={record?.id}>
+            <Typography style={{ textAlign: 'center' }}>
+              {record?.Product.Size}
+            </Typography>
+          </Row>
+        );
+      },
+    },
+    {
+      title: 'Số lượng mua',
+      dataIndex: 'Quantity',
+    },
+  ];
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth='xl'
+      aria-labelledby='alert-dialog-title'
+      aria-describedby='alert-dialog-description'
+    >
+      <DialogTitle id='alert-dialog-title'>{' Chi Tiết đơn hàng'}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id='alert-dialog-description'>
+          <Row>
+            <Typography variant='body1' style={{ marginRight: '50px' }}>
+              Ngày mua: {dataPayment?.orderTime}
+            </Typography>
+            <Typography variant='body1'>
+              Ngày chuyển: {dataPayment?.shipTime}
+            </Typography>
+          </Row>
+          <Row>
+            <Typography variant='body1' style={{ marginRight: '50px' }}>
+              Tổng tiền:{' '}
+              {dataPayment?.total.toLocaleString('it-IT', {
+                style: 'currency',
+                currency: 'VND',
+              })}
+            </Typography>
+          </Row>
+          <TableCustom
+            dataSource={JSONbig.parse(dataPayment?.listItem)}
+            columns={columns}
+            noColumnIndex
+          />
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color='primary' autoFocus>
+          Đóng
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default DialogDetailPayment;
