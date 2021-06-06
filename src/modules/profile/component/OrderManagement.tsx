7ce63dc2f527868bad181d9174d94c75136631f1
datanh14 <37982692+datanh14@users.noreply.github.com>
2021-06-06 23:47:43 +0700

    lên

--- src/modules/profile/component/OrderManagement.tsx
+++ src/modules/profile/component/OrderManagement.tsx
@@ -1,44 +1,74 @@
-import React, { useState } from "react";
-import "antd/dist/antd.css";
-import { Layout, Spin, Table } from "antd";
-import { ACCOUNTS, some, SUCCESS_CODE } from "../../../constants/constants";
-import JSONbig from "json-bigint";
-import { getOrder } from "../api/Order";
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Spin, Table } from 'antd';
+import { ACCOUNTS, some, SUCCESS_CODE } from '../../../constants/constants';
+import JSONbig from 'json-bigint';
+import { getOrder } from '../api/Order';
+import DialogDetailPayment from './DialogDetailPayment';
+import { IconButton } from '@material-ui/core';
+import { Row } from '../../common/Elements';
+import FindInPageIcon from '@material-ui/icons/FindInPage';
+import { GREY_600 } from '../../../assets/theme/colors';
 const { Content } = Layout;
-
-const columns = [
-  {
-    title: "Mã đơn hàng",
-    dataIndex: "id",
-    key: "id",
-  },
-  {
-    title: "Ngày mua",
-    dataIndex: "orderTime",
-    key: "orderTime",
-  },
-  {
-    title: "Sản phảm mua",
-    // dataIndex: "listItem",
-    // key: "listItem",
-  },
-  {
-    title: "Trạng thái đơn hàng",
-    key: "status",
-    dataIndex: "status",
-  },
-  {
-    title: "Tổng tiền",
-    key: "total",
-    dataIndex: "total",
-  },
-];
 
 const OrderManagement = () => {
   const [dataUser, setDataUser] = useState<some>(
-    JSONbig.parse(localStorage.getItem(ACCOUNTS) || "{}")
+    JSONbig.parse(localStorage.getItem(ACCOUNTS) || '{}')
   );
   const [dataOrder, setOrder] = React.useState<any>();
+  const [open, setOpen] = React.useState(false);
+  const [detailPayment, setDetailPayment] = React.useState<any>();
+  const columns = [
+    {
+      title: 'Mã đơn hàng',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Ngày mua',
+      dataIndex: 'orderTime',
+      key: 'orderTime',
+    },
+    {
+      title: 'Sản phảm mua',
+      // dataIndex: "listItem",
+      // key: "listItem",
+    },
+    {
+      title: 'Trạng thái đơn hàng',
+      key: 'status',
+      dataIndex: 'status',
+    },
+    {
+      title: 'Tổng tiền',
+      key: 'total',
+      dataIndex: 'total',
+    },
+    {
+      title: 'Xem',
+      dataIndex: 'id',
+      render: (text: any, record: any) => {
+        return (
+          <Row key={record?.id}>
+            <IconButton
+              onClick={() => {
+                setDetailPayment(record);
+                handleClickOpen();
+              }}
+            >
+              <FindInPageIcon />
+            </IconButton>
+          </Row>
+        );
+      },
+    },
+  ];
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   React.useEffect(() => {
     const fetchUserId = async () => {
       try {
@@ -57,43 +87,53 @@ const OrderManagement = () => {
   if (dataOrder === undefined) {
     return (
       <Content
-        className="site-layout-background"
+        className='site-layout-background'
         style={{
-          margin: "25px 25px",
-          padding: "50px 50px",
-          backgroundColor: "white",
-          height: "600px",
+          margin: '25px 25px',
+          padding: '50px 50px',
+          backgroundColor: 'white',
+          height: '600px',
         }}
       >
-        <Spin size="large" style ={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}/>
+        <Spin
+          size='large'
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        />
       </Content>
     );
   }
   return (
-    <Content
-      className="site-layout-background"
-      style={{
-        margin: "25px 25px",
-        padding: "50px 50px",
-        backgroundColor: "white",
-      }}
-    >
-      <Table
-        columns={columns}
-        dataSource={dataOrder?.detail}
-        pagination={{
-          defaultPageSize: 5,
-          position: ["bottomCenter"],
-        }}
+    <div>
+      <Content
+        className='site-layout-background'
         style={{
-          margin: "25px 25px",
+          margin: '25px 25px',
+          padding: '50px 50px',
+          backgroundColor: 'white',
         }}
+      >
+        <Table
+          columns={columns}
+          dataSource={dataOrder?.detail}
+          pagination={{
+            defaultPageSize: 5,
+            position: ['bottomCenter'],
+          }}
+          style={{
+            margin: '25px 25px',
+          }}
+        />
+      </Content>
+      <DialogDetailPayment
+        open={open}
+        handleClose={handleClose}
+        dataPayment={detailPayment}
       />
-    </Content>
+    </div>
   );
 };
 export default OrderManagement;
