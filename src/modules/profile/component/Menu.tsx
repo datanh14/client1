dcc84a69717308018f94d40a72e3ffa61bc4d4e8
datanh14 <datanh14@gmail.com>
2021-06-07 15:52:45 +0700

    Update Menu.tsx

--- src/modules/profile/component/Menu.tsx
+++ src/modules/profile/component/Menu.tsx
@@ -10,13 +10,13 @@ import {
   ShoppingCartOutlined,
   StarOutlined,
   UserOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu } from "antd";
-import "antd/dist/antd.css";
-import JSONbig from "json-bigint";
-import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import { ACCOUNTS, some } from "../../../constants/constants";
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import JSONbig from 'json-bigint';
+import React, { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { ACCOUNTS, some } from '../../../constants/constants';
 
 const { Sider } = Layout;
 const SubMenu = () => {
@@ -28,69 +28,69 @@ const SubMenu = () => {
       <Layout
         className="site-layout-background"
         style={{
-          margin: "25px 25px",
-          backgroundColor: "white",
+          margin: '25px 25px',
+          backgroundColor: 'white',
         }}
       >
         <Sider width={250} className="site-layout-background">
           <Menu
             mode="inline"
             defaultSelectedKeys={pathnameDefault}
-            style={{ height: "100%", borderRight: 0 }}
+            style={{ height: '100%', borderRight: 0 }}
           >
             <Menu.Item key={`/customer/account/edit`}>
-              {" "}
+              {' '}
               <NavLink to={`/customer/account/edit`}>
                 <UserOutlined />
                 Thông tin tài khoản
               </NavLink>
             </Menu.Item>
-            <Menu.Item key={`/customer/notification`}>
+            {/* <Menu.Item key={`/customer/notification`}>
               {" "}
               <NavLink to={`/customer/notification`}>
                 <NotificationOutlined />
                 Thông báo của tôi
               </NavLink>
-            </Menu.Item>
+            </Menu.Item> */}
 
             <Menu.Item key="/sales/order/history">
-              {" "}
+              {' '}
               <NavLink to="/sales/order/history">
                 <FormOutlined />
                 Quản lý đơn hàng
               </NavLink>
             </Menu.Item>
             <Menu.Item key={`/customer/address`}>
-              {" "}
+              {' '}
               <NavLink to={`/customer/address`}>
                 <EnvironmentOutlined />
                 Sổ địa chỉ
               </NavLink>
             </Menu.Item>
-            <Menu.Item key={`/customer/paymentcard`}>
-              {" "}
+            {/* <Menu.Item key={`/customer/paymentcard`}>
+              {' '}
               <NavLink to={`/customer/paymentcard`}>
                 <CreditCardOutlined />
                 Thông tin thanh toán
               </NavLink>
-            </Menu.Item>
-            <Menu.Item key="9">
-              {" "}
+            </Menu.Item> */}
+            {/* <Menu.Item key="9">
+              {' '}
               <ShoppingCartOutlined />
               Sản phẩm mua sau
-            </Menu.Item>
+            </Menu.Item> */}
             <Menu.Item key="/chatbot">
               <NavLink to="/chatbot">
                 <QuestionCircleOutlined />
                 Hỏi đáp
               </NavLink>
             </Menu.Item>
-            <Menu.Item key="/customer/coupons">
+            {/* <Menu.Item key="/customer/coupons">
               <NavLink to="/customer/coupons">
                 <DollarCircleOutlined />
                 Mã giảm giá
               </NavLink>
-            </Menu.Item>
+            </Menu.Item> */}
           </Menu>
         </Sider>
       </Layout>
